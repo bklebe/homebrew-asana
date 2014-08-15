4d0729cf47fd1171c417f675cf34a89eb81c73f8
memerelics <takuya21hashimoto@gmail.com>
2014-08-16 03:40:05 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+Homebrew-asana
+===========================================
+
+This formula provides [asana command line client](https://github.com/memerelics/asana).
+
+
+How to use this formula
+===========================================
+
+    $ brew tap memerelics/asana
+    $ brew install asana
---
+++ asana.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+ASANA_VERSION = '0.0.1'
+class Asana < Formula
+  homepage 'https://github.com/memerelics/asana'
+  url 'https://github.com/memerelics/asana.git', tag: "v#{ASANA_VERSION}"
+  version ASANA_VERSION
+
+  depends_on 'go' => :build
+
+  def install
+    ENV['GOPATH'] = buildpath
+    system 'go', 'get', 'github.com/codegangsta/cli'
+    system 'go', 'get', 'github.com/memerelics/asana'
+    system 'go', 'build', '-o', 'asana'
+    bin.install 'asana'
+  end
+end
