4673e2b3b78d97268b77f69954b2ef6b870e199f
thash <hash@hzl.jp>
2019-08-23 22:57:15 +0900

    bump version to 0.2.1

--- asana.rb
+++ asana.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-ASANA_VERSION = '0.2.0'
+ASANA_VERSION = '0.2.1'
 class Asana < Formula
   homepage 'https://github.com/thash/asana'
   url 'https://github.com/thash/asana.git', tag: "v#{ASANA_VERSION}"
