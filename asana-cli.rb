bc05e9ac4d7866622068cc2d521ac9e1e2501fa9
Beatrix Klebe <bklebe@mbta.com>
2023-07-17 11:16:47 -0400

    feat: regenerate formula

---
+++ asana-cli.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class AsanaCli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/bklebe/homebrew-asana-cli/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "360803d6beb00d17e8509cc3665b73dd05732fc1ccdcfed2a0dcf411c600073b"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+    bin.install "asana-cli" => "asana"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test asana-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
