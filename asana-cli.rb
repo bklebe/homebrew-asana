4bfed351635491ba1fe4bda73c687b643637b51f
Beatrix Klebe <bklebe@mbta.com>
2023-07-17 11:52:36 -0400

    feat: add hash and simplify install

--- asana-cli.rb
+++ asana-cli.rb
@@ -1,11 +1,12 @@
 class AsanaCli < Formula
   url "https://github.com/bklebe/asana/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "94fbc64ef7e47aea49b22adf0309abf31babd73e50316c1f74a38e9ff53bfa75"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(output: "bin/asana", ldflags: "-s -w")
-    bin.install "bin/asana"
+    system "go", "build", *std_go_args(output: "asana", ldflags: "-s -w")
+    bin.install "asana"
   end
 
   test do
